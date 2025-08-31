d19cc728fd6c840bd1c7f3fa313a7f72e91039ea
Dharmendra <dharmendrathakur.dev@gmail.com>
2025-08-31 13:20:03 +0545

    changes

--- src/components/pages/promoShare/PromoShare.jsx
+++ src/components/pages/promoShare/PromoShare.jsx
@@ -14,6 +14,12 @@ function PromoShare() {
 
   return (
     <div>
+         <h2 className="text-white text-2xl md:text-3xl font-semibold text-center mb-2">
+        Promoshare Lorem Ipsum Dolor
+        </h2>
+        <p className="text-white/60 text-sm text-center mb-6">
+          Lorem ipsum dolor sit amet consectetur.
+        </p>
     <div className="grid grid-cols-1 justify-self-center md:grid-cols-2 lg:grid-cols-3 gap-6">
 
         {
