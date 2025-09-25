249686071aad8eb3beaf954518b581b78fc44014
Dharmendra Thakur <dharmendrathakur.dev@gmail.com>
2025-09-25 14:18:21 +0545

    changes

---
+++ src/components/pages/IPOpipeline/IpoPipeLine.jsx
@@ -0,0 +1,12 @@
+import { useEffect } from "react";
+
+const Otc = () => {
+  useEffect(() => {
+    window.open("https://www.sebon.gov.np/uploads/2025/09/02/l6VJedVMj24s6NucgfWt93s0Ft4pXLmUXpygwbgf.pdf", '_blank');
+    window.location.href = '/';
+  }, []);
+  return <></>;
+};
+
+export default Otc;
+
--- src/components/utilities/loading/Loading2.jsx
+++ src/components/utilities/loading/Loading2.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import styles from './Loading2.module.css';
+
+import loadingImgGIF from '../../../assets/img/promoquityArow.gif'
 
 function Loading2() {
   return (
-    
-<div className={styles.loader}></div>
+  <div className="loadingGif">
+    <img className='w-[300px]' src={loadingImgGIF} alt="" />
+  </div>
+
   )
 }
 
